e939b0cf68fe2922e7ecee60d4612cf9f9974073
Tenzin Kunchok <tenkunchok012511@gmail.com>
2024-05-23 13:04:59 -0400

    components

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Tenzin Kunchok</p>
+        </footer>
+    );
+}
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+function Header(){
+    return(
+       <header>
+            <h1>My Website</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Service</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+       </header>
+        
+    )
+}
+export default Header
